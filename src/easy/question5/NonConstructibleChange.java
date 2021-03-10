5d516ed2c71d9840ab748458c8015122043866e7
Waqar Yunus Shaikh <waqaryshaikh@gmail.com>
2021-03-10 08:56:06 +0530

    BranchSums

--- src/easy/question5/NonConstructibleChange.java
+++ src/easy/question5/NonConstructibleChange.java
@@ -2,6 +2,28 @@ package easy.question5;
 
 import java.util.Arrays;
 
+/**
+ *
+ * Create a variable to store the amount of change that you can currently create
+ * up to. Sort all of your coins, and loop through them in ascending order. At
+ * every iteration, compare the current coin to the amount of change that you can
+ * currently create up to. Here are the two scenarios that you'll encounter:
+ * <ul>
+ *   <li>
+ *     The coin value is <b>greater</b> than the amount of change that you can
+ *     currently create plus 1.
+ *   </li>
+ *   <li>
+ *     The coin value is <b>smaller than or equal to</b> the amount of change that
+ *     you can currently create plus 1.
+ *   </li>
+ * </ul>
+ *
+ * In the first scenario, you simply return the current amount of change that you
+ * can create plus 1, because you can't create that amount of change. In the
+ * second scenario, you add the value of the coin to the amount of change that
+ * you can currently create up to, and you continue iterating through the coins.
+ */
 public class NonConstructibleChange {
 
     public static void main(String[] args) {
